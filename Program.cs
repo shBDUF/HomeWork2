3c99505b6c97081af7e94b735f35e3ffc370d165
Grigoriy <inpuct@mail.ru>
2023-05-01 16:24:28 +0300

    first ex

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+if((numOne/100)>=1 && (numOne/1000)<1)
+{
+    Console.WriteLine($"Вторая цифра числа {numOne} равна {numOne/10%10}");
+}
+else
+{
+    Console.WriteLine("Это не трёхзначное число");
+}
